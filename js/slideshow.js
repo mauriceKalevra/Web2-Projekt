8037ec4cce80aa446ea758073dc3333b997bb40c
Maurice <Mori.r@web.de>
2021-11-07 18:54:49 +0100

    more chamges

--- js/slideshow.js
+++ js/slideshow.js
@@ -2,17 +2,9 @@
 $(window).on("load", function () {
   // List all images that shall be used as background images.
   var background_images = [
-    "hospice-1821429_1920.jpg",
-    "baker-858401_1920.jpg",
-    "barber-shop-5212059_1280.jpg",
-    "welder-3018425_1920.jpg",
-    "construction-664970_1280.jpg",
-    "firefighters-1717916_1920.jpg",
-    "surgery-1807541_1920.jpg",
-    "construction.jpg",
-    "glass.jpg",
-    "workers_1920×1015.jpg",
-    "student-3891017_1920.jpg",
+    "pic1.jpg",
+    "pic2.jpg",
+    "pic4.jpg"
   ]
 
   // Add path "img/background-images/" to every image name to store the path.
